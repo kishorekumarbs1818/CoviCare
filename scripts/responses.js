c1063c88315fcf4ec0c906d42326a2c9c59311e1
kishorekumarbs1818 <73348073+kishorekumarbs1818@users.noreply.github.com>
2021-10-06 11:54:46 +0530

    Add files via upload

---
+++ scripts/responses.js
@@ -0,0 +1,19 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "hi") {
+        return "Hello, How Can We Help You?";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
